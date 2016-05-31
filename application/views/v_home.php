499d39eae5344a7991f36e83f66ba92fadaddc6d
MichaelChrisdian <michael.chrisdian@yahoo.com>
2016-05-31 08:03:00 +0700

    31-05-2016

---
+++ application/views/v_home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+		<title>WAKASA</title>
+	</head>
+	<body>
+		HOME PAGE
+	</body>
+</html>
