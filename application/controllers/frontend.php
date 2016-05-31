130937ecd91027bc5a81650035fce15989254ffd
MichaelChrisdian <michael.chrisdian@yahoo.com>
2016-05-31 08:11:35 +0700

    31-05-2016_v1
    controller
    view

--- application/controllers/frontend.php
+++ application/controllers/frontend.php
@@ -4,5 +4,17 @@ class Frontend extends CI_Controller {
 	public function index(){
 		$this->load->view('v_home');
 	}
+	
+	public function home(){
+		$this->load->view('v_home');
+	}
+	
+	public function searchGeneral(){
+		$this->load->view('v_search_general');
+	}
+	
+	public function searchProduct(){
+		$this->load->view('v_search_product');
+	}
 }
 ?>
---
+++ application/views/v_search_product.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+		<title>WAKASA</title>
+	</head>
+	<body>
+		SEARCH PRODUCT PAGE
+	</body>
+</html>
