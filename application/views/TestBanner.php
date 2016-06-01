396a23bdf78febdc5124c45da08ad1d22a6f3c15
kentwilliam95 <kentwilliam95@gmail.com>
2016-06-02 02:12:06 +0700

    Header Banners

--- application/views/TestBanner.php
+++ application/views/TestBanner.php
@@ -2,25 +2,32 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("asset/")?>">
 <style>
  @media only screen and (max-width: 360px)
  {
-	.col1
-	{
-		position:absolute;
-		top:0px;right:0px;
-	}
-	.f_size1
-	{
-		font-size:2em;
+	 #tulisanBawahLogo
+	 {
+		 position:absolute;
+		 display:inline;
+		 width:150px;
+		 top:130%;
+		 left:20%
+	 }
+	 #kanan1
+	 {float:right;
+	 width:100px;margin-left:70px;}
+		.col1
+		{
+			position:absolute;
+			top:9%;
 		}
-
-	
-        .emailImage{
-			margin:2.5% 2% 2% 2%;
-            height:auto !important;
-            min-width:100px !important;
-            width: 100% !important;
+        .logo{
+			width: 220px;
+			height:auto;
+			left:30px;
+			top:5px;
+		
         }
 		.headerBG
 		{
@@ -30,21 +37,9 @@
 			width:100%;
 			min-height:85px;
 		}
-		.IsiPageBG
-		{
-			width:100%;height:auto;
-			padding:0px;margin-left:10%;
-		}
-
- }
- 
- 
- @media only screen and (min-width:400px)
- {
-	 .IsiPageBG
-	{
-		margin-left:5%;
-	}
+		.f_size1
+		{text-align:center;}
+		
 
  }
 body
@@ -54,18 +49,18 @@ body
 }
 .headerBG
 {
+	position:absolute;
 	padding:0px;
 	margin:0px;
 	background-color:#005ce6;
 	width:100%;
-	min-height:100px;
+	height:100px;
 }
 .logo
 {
-	margin:1.3% 2% 2% 2%;
+	position:absolute;
 	max-width: 250px;
-    height: auto;
-	float:left;
+	margin:1.3% 2% 2% 2%;
 }
 .col1
 {
@@ -112,12 +107,12 @@ body
 </head>
 <body>
 <div class="headerBG">
-	<img class="emailImage logo" src="<?php echo base_url("asset/image/HasilSearchProduk/wakasalogo.png")?>"></img>
+	<img class="logo" src="<?php echo base_url("asset/image/HasilSearchProduk/wakasalogo.png")?>"></img>
 	<div class="col1">
 		<div class="col1_1">
-		<div class="margl7 f_size1"><Strong style="color:yellow"><i>ENG</i>|<i>ID</i></strong></div>
-		<p class="col1_1">welcome back,&nbsp <span style="color:white;display:inline">Mr Vincent</span></p>
-		<div class="margl6"><a href="#" style="color:yellow">[ Logout ]</a></div>
+		<div class="margl7 f_size1" id="kanan1"><Strong style="color:yellow"><i>ENG</i>|<i>ID</i></strong></div>
+		<p class="col1_1 f_size1" id="tulisanBawahLogo">welcome back,&nbsp <span class="f_size1" style="color:white;display:inline">Mr Vincent</span></p>
+		<div class="margl6 f_size1" id="kanan1"><a href="#" style="color:yellow">[ Logout ]</a></div>
 		</div>
 	</div>
 </div>
