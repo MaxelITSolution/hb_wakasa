614c0d581dc65793fec407c73e5129c8dd2bfacd
Kristian Indra <kristian.indra.1412@gmail.com>
2016-07-15 16:31:01 +0700

    config
    jangan lupa ubah site_url di config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.212.132/maxel/helobrand/hb_wakasa/';
+$config['base_url'] = 'http://localhost/maxel/helobrand/hb_wakasa/';
 
 /*
 |--------------------------------------------------------------------------
