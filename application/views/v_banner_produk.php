1704b053a859c24192d7496b0388cca91c6f287f
Kristian Indra <kristian.indra.1412@gmail.com>
2016-07-15 13:54:45 +0700

    fix pencarian produk

--- application/views/v_banner_produk.php
+++ application/views/v_banner_produk.php
@@ -30,31 +30,59 @@
 	        <?php } ?>
 		</select>
 
-		<select class="selectpicker" id="model_select" name="model_select" onchange="setModel();">
-			<option value="">By Model</option>
-	        <?php foreach ($models->result() as $row) { ?>
-	         	<option class="<?=$row->make;?>" value="<?=$row->model;?>"><?=$row->model;?></option>
-	        <?php } ?>
-		</select>
+    <select class="selectpicker model_select" id="model_select" name="model_select" onchange="setModel();">
+      <option value="">By Model</option>
+          <?php foreach ($models->result() as $row) { ?>
+             <option class="<?=$row->make;?>" value="<?=$row->model;?>"><?=$row->model;?></option>
+          <?php } ?>
+    </select>
+
+    <?php 
+      foreach ($models->result() as $row) {
+        $arr_make[$row->make] = "1"; 
+      } 
+      foreach ($arr_make as $key => $m) {
+        ?>
+          <select class="selectpicker model_select <?php echo $key?>" id="model_select" name="model_select" onchange="setModel();" style="display:none">
+            <?php foreach ($models->result() as $row) { ?>
+              <?php if ($row->make == $key) { ?>
+                <option value="<?=$row->model;?>"><?=$row->model;?></option>
+              <?php } ?>
+            <?php } ?>
+          </select>          
+        <?php
+      }
+      
+    ?>
+        
 	</form>
 
 	<script type="text/javascript">
 
+    var list_model = [];
+    var ctr = 0;
+    <?php $ctr = 0; ?>
+    
 		//CODING UNTUK FILTER YANG INI
 		$('#make_select').on('change', function(){
 			setMake();
 			//DISPLAY ALL
-			$('#model_select').find('option').each(function() {
-				$(this).show();
-			});
+      var optVal= $("#make_select").val();
+      
+      $('.model_select').hide();
+			$('.model_select').prop( "disabled", true );
+      $('.model_select.'+optVal).show();
+      $('.model_select.'+optVal).prop( "disabled", false );
 
+    
 			//DISPLAY BY MAKE
-			var optVal= $("#make_select option:selected").val();
-			$('#model_select').find('option').each(function() {
-				if ($(this).attr('class')!=optVal){
-					$(this).hide();
-				}
-			});
+//      var optVal= $("#make_select option:selected").val();
+//  		var optVal= $("#make_select").val();
+//			$('#model_select').find('option').each(function() {
+//				if ($(this).attr('class') != optVal){
+//					$(this).hide();
+//				}
+//			});
 		});
 
 
@@ -77,14 +105,18 @@
 	        };
 		}*/
 		function setMake(){
-			var getMake = document.getElementById('make_select').value;
+//			var getMake = document.getElementById('make_select').value;
+      var getMake = $('#make_select').val();
 			var getMake = getMake.replace(/'/g, "\\'");
-			document.getElementById('txt_make').value = getMake;
+//      document.getElementById('txt_make').value = getMake;
+			$('#txt_make').val(getMake);
 		}
 		function setModel(){
-			var getModel = document.getElementById('model_select').value;
+//      var getModel = document.getElementById('model_select').value;
+			var getModel = $('#model_select').val();
 			var getModel = getModel.replace(/'/g, "\\'");
-			document.getElementById('txt_model').value = getModel;
+//      document.getElementById('txt_model').value = getModel;
+			$('#txt_model').val(getModel);
 		}
 
 	</script>
